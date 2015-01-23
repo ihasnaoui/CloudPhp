d8275ead623e3d6bdd97cba312972f2cb24fab86
ihasnaoui <ihasnaoui.ensit@gmail.com>
2015-01-23 10:53:32 +0100

    Update modifierM.php

--- modifierM.php
+++ modifierM.php
@@ -79,8 +79,8 @@
 <?php
 if($_POST['submit'])
 {
- $connect=mysql_connect('127.11.6.2','adminstqScTq',' gKKHC6bRL9Gv',3306);
-    			mysql_select_db('phpcloud');
+ $connect=mysql_connect('127.11.6.2','adminsnUnrR1','S8F-UUCpn9Mw',3306);
+    			mysql_select_db('cloudphp');
   
   $ncin=$_POST['ncin'];
   $nom=$_POST['nom'];
